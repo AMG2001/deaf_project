7ca7da45520fc29b81dbd220629c1c0560844bcd
Mohamad Amgad <mohamadamgad09@gmail.com>
2023-05-15 15:01:02 +0300

    firebase code added

--- deaf_project/lib/main.dart
+++ deaf_project/lib/main.dart
@@ -17,9 +17,13 @@ import 'package:deaf_project/easy_20meetapp/generatedsplashwidget/GeneratedSplas
 import 'package:deaf_project/easy_20meetapp/preparedMeeting.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(Easy_20MeetApp());
 }
 
